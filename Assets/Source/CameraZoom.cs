fb4562a3ae9d6c6634164b0e9d5f0ff689cbb62a
Javier Martinez <hoogmin@gmail.com>
2017-12-01 22:20:33 -0500

    Began movement and zooming

---
+++ Assets/Source/CameraZoom.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+
+public class CameraZoom : MonoBehaviour
+{
+    private float _minCameraZoom = 5f;
+    private float _maxCameraZoom = 10f;
+
+
+    private void SwitchCameraZoom()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (Camera.main.orthographicSize == _minCameraZoom)
+                Camera.main.orthographicSize = _maxCameraZoom;
+            else
+                Camera.main.orthographicSize = _minCameraZoom;
+        }
+           
+    }
+
+    private void Update()
+    {
+        SwitchCameraZoom();
+    }
+
+}
---
+++ Assets/Source/PlayerMov.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+
+public class PlayerMov : MonoBehaviour
+{
+    private Rigidbody2D _rb2d;
+    private float _speed = 8f;
+
+
+    private void Awake()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+            _rb2d.velocity += Vector2.up * _speed;
+
+        if (Input.GetKey(KeyCode.A))
+            _rb2d.velocity += Vector2.left * _speed;
+
+        if (Input.GetKey(KeyCode.S))
+            _rb2d.velocity += Vector2.down * _speed;
+
+        if (Input.GetKey(KeyCode.D))
+            _rb2d.velocity += Vector2.right * _speed;
+    }
+
+    private void LimitSpeed()
+    {
+        if (_rb2d.velocity.x >= _speed || _rb2d.velocity.y >= _speed)
+        {
+            _rb2d.velocity = new Vector2(_speed, _speed);
+        }
+    }
+}
